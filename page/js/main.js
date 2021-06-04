b17ea16a9ff8833a077cc41fcdf5df989aba4f42
fgl27 <fglfgl27@gmail.com>
2021-06-04 10:15:24 -0300

    Update page
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- page/js/main.js
+++ page/js/main.js
@@ -1,28 +1,71 @@
 
 let Element_obj = {
-    pot: {
-        type: 'input',
+    pot_nominal_array: {
+        elem: 'input',
         innerHTML: 'Potência nominal da matriz (W/m²)',
         value: 1000,
-        alert: 'Aceita somente números'
+        type: 'number',
+        step: '10',
+        help: 'O valor nominal total da matriz fotovoltaica instalada em W/m²'
     },
-    // angulo: {
-    //     type: 'input',
-    //     innerHTML: 'Ângulo painel',
-    //     value: 20
-    // },
+    perda: {
+        elem: 'input',
+        innerHTML: 'Perdas do sistema (%)',
+        value: 14,
+        type: 'number',
+        step: '1',
+        help: 'As perdas no sistema que não são explicitamente modeladas, que incluem os impactos na potência final de sujeira, sombreamento, cobertura de neve, incompatibilidade, fiação, conexões, degradação induzida pela luz, classificação da placa de identificação, idade do sistema e disponibilidade operacional'
+    },
+    coef_temp: {
+        elem: 'input',
+        innerHTML: 'Coeficiente de temperatura de potência (%/°C)',
+        value: -0.35,
+        type: 'number',
+        step: '0.01',
+        help: 'A eficiência da matriz diminua a uma taxa linear em função do aumento da temperatura, governada pelo coeficiente de temperatura do painel'
+    },
+    cc_ca: {
+        elem: 'input',
+        innerHTML: 'Conversão CA-CC (Eficiencia %)',
+        value: 96,
+        type: 'number',
+        step: '1',
+        help: 'O modelo proposto utiliza uma simples conversão baseada na eficiência do inversor'
+    },
+    angulo: {
+        elem: 'input',
+        innerHTML: 'Ângulo painel',
+        value: 20
+    },
     button: {
-        type: 'button',
+        elem: 'button',
         innerHTML: 'Calcular'
-    }
+    },
+    divider: {
+        elem: 'divider',
+    },
+    order_base: [
+        'pot_nominal_array',
+        'divider',
+        'coef_temp',
+        'divider',
+        'perda',
+        'divider',
+        'cc_ca',
+        'button'
+    ]
 };
 
 const fun_obj = {
+    divider: function() {
+        const div_base = document.createElement('div');
+        div_base.className = 'divider';
+        inputsDiv.appendChild(div_base);
+    },
     input: function(prop) {
         let obj = Element_obj[prop];
 
         const div_base = document.createElement('div');
-        div_base.className = 'divBase';
         div_base.setAttribute('id', prop);
 
         const div_text = document.createElement('div');
@@ -32,25 +75,22 @@ const fun_obj = {
 
         const div_input = document.createElement('input');
         div_input.className = 'inputsInput';
+        div_input.type = obj.type;
+        div_input.step = obj.step;
         if (obj.value) div_input.value = obj.value;
         div_input.setAttribute('id', prop + '_Input');
         div_input.onchange = function() {
-            if (this.value !== '' && this.value) {
-                if (!isNaN(this.value)) {
-
-                    obj.value = parseInt(this.value);
-
-                } else {
-
-                    this.value = '';
-                    alert(obj.innerHTML + '\n' + obj.alert);
-
-                }
-            }
+            obj.value = this.value;
         };
 
         div_base.appendChild(div_text);
         div_base.appendChild(div_input);
+
+        const div_help = document.createElement('div');
+        div_help.setAttribute('id', prop + '_Help');
+        div_help.className = 'tooltip ' + (obj.help ? '' : 'opacityZero');
+        div_help.innerHTML = ' ? <span class="tooltiptext">' + obj.help + '</span>' //+ (obj.help ? '<span class="tooltiptext">Tooltip text</span>' : '');
+        div_base.appendChild(div_help);
 
         inputsDiv.appendChild(div_base);
     },
@@ -67,7 +107,7 @@ const fun_obj = {
                 i = 0,
                 len = obj.length,
                 ang = 20,//Element_obj.angulo.value * (Math.PI / 180),
-                pm0 = Element_obj.pot.value,
+                pm0 = Element_obj.pot_nominal_array.value,
                 calcPot_Temp = 0.0,
                 mes,
                 dia,
@@ -139,6 +179,7 @@ const fun_obj = {
             total_max = 0,
             base_div_text = 'Mês',
             temp_total,
+            CC_CA = Element_obj.cc_ca.value / 100000,
             monclick = function(prop1, prop2) {
                 fun_obj.Resultado(prop1, prop2);
             };
@@ -198,7 +239,7 @@ const fun_obj = {
         div_result_value.className = 'result_value';
         div_result_value.setAttribute('id', 'result_value_' + base_id);
 
-        div_result_value.innerHTML = base_div_text + '<br>kWh';
+        div_result_value.innerHTML = base_div_text + '<br>kWh (CA)';
         div_result_holder.appendChild(div_result_value);
 
         for (let prop in obj) {
@@ -233,7 +274,7 @@ const fun_obj = {
                 div_result_value.className = 'result_value';
                 div_result_value.setAttribute('id', 'result_value_' + prop);
 
-                temp_total = obj[prop].total * CC_CA / 1000;
+                temp_total = obj[prop].total * CC_CA;
                 if (temp_total > total_max) total_max = temp_total;
 
                 div_result_value.innerHTML = prop + '<br>' + temp_total.toFixed(2);
@@ -253,12 +294,12 @@ const fun_obj = {
         div_result_value.className = 'result_value';
         div_result_value.setAttribute('id', 'result_value_end');
 
-        div_result_value.innerHTML = 'Total<br>' + (obj.total * CC_CA / 1000).toFixed(2);
+        div_result_value.innerHTML = 'Total<br>' + (obj.total * CC_CA).toFixed(2);
         div_result_holder.appendChild(div_result_value);
 
         div_result_note = document.createElement('div');
         div_result_note.className = 'result_note';
-        div_result_note.setAttribute('id', 'result_note_' + prop);
+        div_result_note.setAttribute('id', 'result_note_div');
 
         if (isMonth) {
 
@@ -278,9 +319,11 @@ const fun_obj = {
                 for (let prop in obj) {
 
                     if (prop !== 'total')
-                        mgetElementById('graf_' + prop).style.height = (((obj[prop].total * CC_CA / 1000) / total_max) * 100) + '%';
+                        mgetElementById('graf_' + prop).style.height = (((obj[prop].total * CC_CA) / total_max) * 100) + '%';
 
                 }
+
+                window.scrollTo(0, document.body.scrollHeight);
 
             },
             100,
@@ -328,9 +371,12 @@ function StartPage() {
     inputsDiv = mgetElementById('inputs');
     resultDiv = mgetElementById('result');
 
-    for (prop in Element_obj) {
-        fun_obj[Element_obj[prop].type](prop);
-    }
+    Element_obj.order_base.forEach(GenDiv);
+
+}
+
+function GenDiv(prop) {
+    fun_obj[Element_obj[prop].elem](prop);
 
 }
 
@@ -342,9 +388,6 @@ const a = -3.47;
 const b = -0.0594;
 const Delta_T = 3;
 const k = 0.0015;
-const gamma = -0.35 / 100;
-const perda = 1 - 0.14;
-const CC_CA = 0.96;
 
 function calcPot(PM0, AOI, DNI, EG_ED, TA, WS) {
     const eb = DNI;
@@ -355,14 +398,14 @@ function calcPot(PM0, AOI, DNI, EG_ED, TA, WS) {
 
     const TC = TM + ((POA / 1000) * Delta_T);
 
-    const PPM = (POA / 1000) * (1 + (gamma * (TC - 25)));
+    const PPM = (POA / 1000) * (1 + ((Element_obj.coef_temp.value / 100) * (TC - 25)));
 
     let PM = 0;
 
     if (POA > 200) PM = PM0 * (PPM - (k * (1000 - POA) / (800)));
     else PM = PM0 * (PPM - (k * (1 - (1 - Math.pow(POA / 200, 4)))));
 
-    return PM * perda;
+    return PM * (1 - (Element_obj.perda.value / 100));
 }
 
 function msetTimeout(fun, timeout, id) {
