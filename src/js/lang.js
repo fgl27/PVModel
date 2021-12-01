d0c7e65aa2ff8263e3d7ed1591bc8c41c9b92ded
fgl27 <fglfgl27@gmail.com>
2021-12-01 20:59:29 -0300

    up changes
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- src/js/lang.js
+++ src/js/lang.js
@@ -172,7 +172,7 @@ const Lang = {
             help: 'O valor de venda do kWh cobrado pela recarga'
         },
         ultra_title: {
-            innerHTML: 'Estações de recarga ultrarrápidos',
+            innerHTML: 'Estações de recarga ultrarrápidas',
         },
         fast_title: {
             innerHTML: 'Estações de recarga rápidas',
